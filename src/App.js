d547510cfb6bf1f41b580c2885a1d7fa381f0086
Minja <minja.milivil@gmail.com>
2018-10-16 17:27:19 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import GoogleMapReact from 'google-map-react';
 import './App.css';
+import Flat from './components/flat.js';
+import Marker from './components/marker.js';
+
 
 class App extends Component {
-  render() {
+  constructor(props){
+    super(props);
+    this.state={
+      flats:[],
+      allFlats:[],
+      selectedFlat:null,
+      search:""
+    };    
+    }
+  
+
+    componentDidMount(){
+      const url="https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json";      
+      fetch(url)
+      .then(response=>response.json())
+      .then((data)=>{    
+         this.setState({
+           flats:data,
+           allFlats:data 
+                  });
+
+      })
+    };
+
+    selectFlat=(flat)=>{
+      this.setState({
+         selectedFlat:flat
+      })
+    }
+
+    handleSearch=(event)=>{
+        this.setState({
+        search:event.target.value,
+        flats:this.state.allFlats.filter((flat)=>new RegExp(event.target.value, "i").exec(flat.name))
+      });
+    }
+  
+    render() { 
+
+    let center={      
+            lat: 48.864716,
+            lng: 2.349014       
+          }
+
+    if(this.state.selectedFlat)  {
+      center={      
+        lat: this.state.selectedFlat.lat,
+        lng: this.state.selectedFlat.lng      
+      }
+
+    }    
+   
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+      <div className="main">
+      
+        <div className="search">   
+          <input type="text" placeholder="Search..." value={this.state.search} onChange={this.handleSearch}  />
+        </div>
+        <div className="flats">
+            {this.state.flats.map((flat)=>{
+              return<Flat key={flat.name} flat={flat} selectFlat={this.selectFlat}/>
+            })}
+        </div>
+      </div>
+      <div className="map">
+      <GoogleMapReact center={center}zoom={13}>       
+       
+            {this.state.flats.map((flat)=>{
+              return<Marker key={flat.name}  lat={flat.lat} lng={flat.lng} text={flat.price}  selected={flat===this.state.selectedFlat}/>
+            })}     
+         
+     </GoogleMapReact>
+      
+      </div>
       </div>
     );
   }
---
+++ src/components/marker.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './marker.css';
+class Marker extends Component {
+  
+    render() { 
+       let classes="marker";
+       if(this.props.selected){
+         classes+=" selected";
+       }
+        return ( <div className={classes}> {this.props.text} </div> );
+    }
+}
+ 
+export default Marker;
